89510218227c0e775ceebddb8cdc2fb9a4d7719f
khacthe <nguyenkhacthe94@gmail.com>
2018-04-11 15:30:00 +0700

    Insertion Sorting

---
+++ InsertionSort.java
@@ -0,0 +1,21 @@
+public class InsertionSort {
+
+    static int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+
+    public static void insertionSort(int[] list) {
+        for (int i = 1; i < list.length; i++) {
+            int currentElement = list[i];
+            int k;
+            for (k = i - 1; k >= 0 && list[k] > currentElement; k--) {
+                list[k + 1] = list[k];
+            }
+            list[k + 1] = currentElement;
+        }
+    }
+
+    public static void main(String[] args) {
+        insertionSort(list);
+        for (int i = 0; i < list.length; i++)
+            System.out.print(list[i] + " ");
+    }
+}
